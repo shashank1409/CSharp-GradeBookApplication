68bb4e78480ff309de51f5fed01763c6a850664c
Shashank Dixit <Shashank.Dixit@Philips.com>
2020-02-10 18:11:11 +0530

    Completed module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,8 +16,34 @@ namespace GradeBook.GradeBooks
             if (Students.Count < 5)
             {
                 throw new InvalidOperationException();
-             //   if()
             }
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            Students.Sort();
+            int count = 0;
+            for (int i = 1; i <=Students.Count; i++)
+            { 
+                if(i%threshold==0)
+                {
+                    count++;
+                }
+                if(count==1)
+                {
+                    return 'D';
+                }
+                else if(count==2)
+                {
+                    return 'C';
+                }
+                else if (count == 3)
+                {
+                    return 'B';
+                }
+                else if (count == 4)
+                {
+                    return 'A';
+                }
+            }
+            
             return 'F';
         }
     }
