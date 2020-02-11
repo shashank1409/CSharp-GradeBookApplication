ffd101a830ec189a4ac740a5b1d0652763eaa8f3
Shashank Dixit <Shashank.Dixit@Philips.com>
2020-02-11 10:03:36 +0530

    Completed module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -46,11 +46,15 @@ namespace GradeBook.UserInterfaces
             else if (parts[2].Equals("ranked"))
                 gradeBook = new RankedGradeBook(name);
             else
+            {
                 Console.WriteLine($" is not a supported type of gradebook, please try again");
+                return;
+            }
+                
             // gradeBook = parts[2];
 
             Console.WriteLine("Created gradebook {0}.", name);
-           // GradeBookUserInterface.CommandLoop(gradeBook);
+            GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
         public static void LoadCommand(string command)
@@ -75,7 +79,7 @@ namespace GradeBook.UserInterfaces
             Console.WriteLine();
             Console.WriteLine("GradeBook accepts the following commands:");
             Console.WriteLine();
-            Console.WriteLine("Create 'Name' - Creates a new gradebook where 'Name' is the name of the gradebook.");
+            Console.WriteLine("Create 'Name' 'Type' - Creates a new gradebook where 'Name' is the name of the gradebook and 'Type' is what type of grading it should use.");
             Console.WriteLine();
             Console.WriteLine("Load 'Name' - Loads the gradebook with the provided 'Name'.");
             Console.WriteLine();
